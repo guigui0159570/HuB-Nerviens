c08e4cae077c296564e889c11a54d230d615b74c
Guillaume CODER <62026254+guigui0159570@users.noreply.github.com>
2020-12-08 13:34:17 +0100

    Page js

---
+++ script.js
@@ -0,0 +1,5 @@
+function redirect(page) {
+	if (page == 'Accueil') {window.location.href=('Accueil.html')}
+	else if (page == 'CAS EP')  {window.location.href=('https://teleservices.ac-lille.fr/login/ct_logon_vk.jsp?CT_ORIG_URL=%2Fsso%2FSSO%3FSPEntityID%3Durn%3Afi%3Aent%3Alille-hdf-ts%3A1.0%26TARGET%3Dhttps%3A%2F%2Fwww.enthdf.fr%2F&ct_orig_uri=%2Fsso%2FSSO%3FSPEntityID%3Durn%3Afi%3Aent%3Alille-hdf-ts%3A1.0%26TARGET%3Dhttps%3A%2F%2Fwww.enthdf.fr%2F')}
+	else if (page == 'CAS PROF') {window.location.href=('https://eduline.ac-lille.fr/eduline_profil/anonyme/logon.html')}
+}
